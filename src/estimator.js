f4b7f9886ce3d3cb4a7221592e11c91711a271c2
johnakon <john.akonya@gmail.com>
2020-04-11 21:11:57 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  var impact = {};
+  var severImpact = {};
+
+  var power = 
+
+  //currently affected people
+  impact.currentlyInfected = data.reportedCases * 10;
+  severImpact.currentlyInfected = data.reportedCases * 50;
+
+  return {
+    data,
+    impact,
+    severImpact
+  };
+};
 
 export default covid19ImpactEstimator;
