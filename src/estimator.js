e7208a0ab10ef7e00af5d8b945f49dee8b64cc74
johnakon <john.akonya@gmail.com>
2020-04-11 22:18:18 +0300

    made changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,25 +1,25 @@
 const covid19ImpactEstimator = (data) => {
-  
-    const days;
-    if(data.periodType === 'days'){
-        days = data.timeToElapse
-    }else if(data.periodType === 'weeks'){
-        days = 7 * data.timeToElapse
-    }
-    if(data.periodType === 'months'){
-        days = 30 * data.timeToElapse
-    }
+  let days;
+  if (data.periodType === 'days') {
+    days = data.timeToElapse;
+  } else if (data.periodType === 'weeks') {
+    days = 7 * data.timeToElapse;
+  }
+  if (data.periodType === 'months') {
+    days = 30 * data.timeToElapse;
+  }
 
-    impact.currentlyInfected = data.reportedCases * 10;
-    severImpact.currentlyInfected = data.reportedCases * 50;
+  impact.currentlyInfected = data.reportedCases * 10;
+  severImpact.currentlyInfected = data.reportedCases * 50;
 
+  const value = Math.trunc(days / 3);
 
-    const value = Math.trunc(days / 3)
-
-    impact.infectionsByRequestedTime = Math.trunc(impact.currentlyInfected * ( 2 ** value))
-    severImpact.infectionsByRequestedTime = Math.trunc(severImpact.currentlyInfected * ( 2 ** value))
-
-
+  impact.infectionsByRequestedTime = Math.trunc(
+    impact.currentlyInfected * 2 ** value
+  );
+  severImpact.infectionsByRequestedTime = Math.trunc(
+    severImpact.currentlyInfected * 2 ** value
+  );
 
   return {
     data: {},
